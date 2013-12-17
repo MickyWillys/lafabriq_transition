62a98ee1acce14a3545ccc9fe6ae3d092cddd190
MickyWillys <fozeum@yahoo.fr>
2013-12-17 09:35:31 +0100

    database missing

--- transition.rb
+++ transition.rb
@@ -1,6 +1,10 @@
-require "sinatra"
-require "shotgun"
+require 'sinatra'
+require 'sinatra/activerecord'
+require 'sqlite3'
+require 'rake'
+require 'shotgun'
+
 
 get "/" do 
-	erb:newformat2	
+	erb:lafabriq_lapage.erb	
 end
